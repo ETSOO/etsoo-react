0cafbefc41aba36dfeb454f379fadaa2c14050ed
Garry Xiao <xz@etsoo.com>
2020-04-10 19:16:46 +0800

    Upgrade Node.js

---
+++ lib/src/bridges/ElectronBridge.d.ts
@@ -0,0 +1,7 @@
+import { IBridge } from './IBridge';
+/**
+ * Electron bridge class
+ * copes with preload.js, contextBridge.exposeInMainWorld
+ * BrowserWindow.webPreferences, contextIsolation: true
+ */
+export declare const AppRuntime: IBridge;
---
+++ lib/src/bridges/IAppData.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Bridge App Data interface
+ */
+export interface IAppData {
+    /**
+     * Application name
+     */
+    name: string;
+    /**
+     * Application version
+     */
+    version: string;
+    /**
+     * Application path
+     */
+    path: string;
+}
