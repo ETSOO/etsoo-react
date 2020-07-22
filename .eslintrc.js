74fc8b06924ba8ab7af2e2264f9f588070d541fb
garryxiao <xz@etsoo.com>
2020-07-23 07:13:10 +0800

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+export default {
+	'env': {
+		'browser': true,
+		'es2020': true
+	},
+	'extends': [
+		'airbnb-base',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 11,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	},
+    settings: {
+        'import/extensions': [".js",".jsx",".ts",".tsx"],
+        'import/parsers': {
+          '@typescript-eslint/parser': [".ts",".tsx"]
+         },
+         'import/resolver': {
+             'node': {
+                 'extensions': [".js",".jsx",".ts",".tsx"]
+             }
+         }
+    }
+}
